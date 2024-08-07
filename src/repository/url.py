d96ce0901c303849897e0f5a9a653dfee8e43fbd
Alejo <alejoczombos@gmail.com>
2024-08-07 15:40:52 -0300

    Create, Update and Delete running

--- src/repository/url.py
+++ src/repository/url.py
@@ -1,25 +1,44 @@
 from ..db import urls_collection
-from ..models.url import URLInDB, URLCreate, URLCollection
+from ..models.url import URLInDB, URLIn, URLCollection
 from bson import ObjectId
 
-async def create_url(url: URLCreate):
-    url_data = url
-    url_created = await urls_collection.insert_one(url_data)
-    print(url_created)
-    return url_created
+async def create_url_db(url_in: URLIn):
+    url = URLIn(**url_in.model_dump())
+    
+    alias_exists = await urls_collection.find({"alias": url.alias}).to_list(1)
+    if alias_exists:
+        return None
+    
+    url_data = url.model_dump(by_alias=True)
+    new_url = await urls_collection.insert_one(url_data)
+    url_created = await urls_collection.find_one({"_id": new_url.inserted_id})
+    
+    return URLInDB(**url_created)
 
-async def update_url(url_id: str, url: URLCreate):
-    url_data = url
-    url_updated = await urls_collection.update_one({"_id": ObjectId(url_id)}, {"$set": url_data})
-    print(url_updated)
-    return url_updated
+async def update_url_db(url_in: URLIn, url_id: str):
+    # convert URLIn to URLInDB and remove None values
+    url = URLIn(**url_in.model_dump())
+    url_data = {attribute: value for attribute, value in url.model_dump(by_alias=True).items() if value is not None}
+    
+    if url_data.get("alias"):
+        alias_exists = await urls_collection.find({"alias": url.alias}).to_list(1)
+        if alias_exists:
+            return None
+    
+    exists = await urls_collection.find_one({"_id": ObjectId(url_id)})
+    if not exists:
+        return None
+    
+    url_updated = await urls_collection.find_one_and_update({"_id": ObjectId(url_id)}, {"$set": url_data}, return_document=True)
+    
+    return URLInDB(**url_updated)
 
-async def delete_url(url_id: str):
+async def delete_url_db(url_id: str) -> bool:
     url_deleted = await urls_collection.delete_one({"_id": ObjectId(url_id)})
-    print(url_deleted)
-    return url_deleted
+    
+    return url_deleted.deleted_count > 0
 
-async def get_all_urls():
+async def get_all_urls_db():
     urls_db = await urls_collection.find().to_list(1000)
     
     for url in urls_db:
@@ -34,19 +53,20 @@ async def get_all_urls():
     
     return URLCollection(urls=urls_db)
 
-async def get_url_by_id_or_alias(id_or_alias: str):
-    url_db: URLInDB = await urls_collection.find_one({
-            "$or": [
-                {"_id": ObjectId(id_or_alias)},
-                {"alias": id_or_alias}
-            ]
-    })
+async def get_url_by_id_or_alias_db(id_or_alias: str):
+    alias_is_valid = ObjectId.is_valid(id_or_alias)
+    
+    if alias_is_valid:
+        url_db: URLInDB = await urls_collection.find_one({"_id": ObjectId(id_or_alias)})
+        return url_db
+    
+    url_db: URLInDB = await urls_collection.find_one({"alias": id_or_alias})
     return url_db
 
-async def get_url_by_id(url_id: str):
+async def get_url_by_id_db(url_id: str):
     url_data = await urls_collection.find_one({"_id": ObjectId(url_id)})
     return URLInDB(**url_data) if url_data else None
 
-async def get_url_by_alias(alias: str):
+async def get_url_by_alias_db(alias: str):
     url_data = await urls_collection.find_one({"alias": alias})
     return URLInDB(**url_data) if url_data else None
--- src/routers/url.py
+++ src/routers/url.py
@@ -1,11 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import RedirectResponse
-from ..db import urls_collection
-from bson import ObjectId
 
-from ..repository.url import create_url, update_url, delete_url, get_url_by_id_or_alias, get_all_urls as get_all_urls_db
-from ..models.url import URLInDB, URLCreate, URLCollection as URLCollectionDB
-from ..schemas.url import URLCreate, URLCollection, URLUpdate, URLDelete, URLResponse, PasswordParam, IdOrAliasParam
+from ..repository.url import create_url_db, update_url_db, delete_url_db, get_url_by_id_or_alias_db, get_all_urls_db
+from ..models.url import URLInDB, URLIn, URLCollection as URLCollectionDB 
+from ..schemas.url import URLCreate, URLCollection, URLUpdate, URLResponse, PasswordParam, IdOrAliasParam
 
 router = APIRouter(
     prefix="/urls",
@@ -15,6 +13,10 @@ router = APIRouter(
 
 @router.get(
     "/", 
+    name="Get all URLs",
+    response_description="List of all URLs",
+    status_code=status.HTTP_200_OK,
+    response_model_by_alias=False,
     response_model=URLCollection
     )
 async def get_all_urls():
@@ -26,29 +28,68 @@ async def get_all_urls():
 @router.get(
     "/{id_or_alias}",
     name="Redirect URL",
-    description="Redirect to the URL",
+    response_description="Redirect to the URL",
+    status_code=status.HTTP_302_FOUND,
+    response_model_by_alias=False,
     )
 async def redirect_to_url(id_or_alias: IdOrAliasParam, password: PasswordParam = None):
-    url_db: URLInDB = await get_url_by_id_or_alias(id_or_alias)
+    url_db: URLInDB = await get_url_by_id_or_alias_db(id_or_alias)
     
     if url_db == None:
-        #Agregar un return con un mensaje de error con codigo de error
-        return {"error": "URL not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="URL not found")
     
     if url_db["password"] and password != url_db["password"]:
-        return {"error": "Invalid password"}
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
     
     return RedirectResponse(url_db["url"])
     
 
-@router.post("/")
-async def create_url():
-    return {"url": "https://www.google.com"}
+@router.post(
+    "/",
+    name="Create URL",
+    response_description="URL created successfully",
+    status_code=status.HTTP_201_CREATED,
+    response_model=URLResponse,
+    response_model_by_alias=False,
+    )
+async def create_url(url: URLCreate):
+    url_created : URLInDB = await create_url_db(url)
+    
+    if url_created is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Alias already exists")
+    
+    URLResponse(**url_created.model_dump())
+    return url_created
 
-@router.put("/{url_id}")
-async def update_url(url_id: int):
-    return {"url_id": url_id}
+@router.put(
+    "/{url_id}",
+    name="Update URL",
+    response_description="URL updated successfully",
+    status_code=status.HTTP_200_OK,
+    response_model=URLResponse,
+    response_model_by_alias=False,
+    )
+async def update_url(url_id: IdOrAliasParam, url: URLCreate):
+    url_updated : URLInDB = await update_url_db(url, url_id)
+    
+    if url_updated is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="URL not found")
+    
+    URLResponse(**url_updated.model_dump())
+    return url_updated
 
-@router.delete("/{url_id}")
-async def delete_url(url_id: int):
-    return {"url_id": url_id}
+@router.delete(
+    "/{url_id}",
+    name="Delete URL",
+    response_description="URL deleted successfully",
+    status_code=status.HTTP_200_OK,
+    response_model=None,
+    response_model_by_alias=False,
+    )
+async def delete_url(url_id: IdOrAliasParam):
+    url_deleted = await delete_url_db(url_id)
+    
+    if not url_deleted:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="URL not found")
+    
+    return {"message": "URL deleted successfully"}

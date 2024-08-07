be2b30f4ae78e451f394fc75d00aabc08094acc9
Alejo <alejoczombos@gmail.com>
2024-08-07 14:28:49 -0300

    Models of URLs

---
+++ src/models/url.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+from pydantic.functional_validators import BeforeValidator
+from datetime import datetime
+from typing import List, Annotated, Optional
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class URLBase(BaseModel):
+    url: str
+    description: Optional[str]
+    alias: Optional[str]
+    expiration: Optional[datetime]
+    password: Optional[str]
+
+class URLCreate(URLBase):
+    pass
+
+class URLInDB(URLBase):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+
+class URLCollection(BaseModel):
+    urls: List[URLInDB]

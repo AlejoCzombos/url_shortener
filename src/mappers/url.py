0f6c23af0cdcecd634220284477a738a33e1608d
Alejo <alejoczombos@gmail.com>
2024-08-09 22:11:12 -0300

    mappers from models to schemas

--- src/mappers/url.py
+++ src/mappers/url.py
@@ -4,10 +4,12 @@ from ..schemas.url import URLResponse, URLCollection
 def url_in_db_to_url_response(url_in_db: URLInDB) -> URLResponse:
     return URLResponse(
         id=url_in_db.id,
-        url=url_in_db.url,
+        original_url=url_in_db.original_url,
+        short_url=url_in_db.short_url,
         description=url_in_db.description,
         alias=url_in_db.alias,
-        expiration=url_in_db.expiration,
+        expires_at=url_in_db.expires_at,
+        created_at=url_in_db.created_at,
         has_password=bool(url_in_db.password),
     )
 
